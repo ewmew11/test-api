dec0b2faf735084e93b53218863684b3014ff110
ewmew11 <ewmew11@gmail.com>
2024-02-17 11:50:01 +0700

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,38 +1,49 @@
 import React, { useEffect, useState } from 'react';
+import axios from 'axios';
 
 const App = () => {
   const [data, setData] = useState(null);
-  const [error, setError] = useState(null);
 
   useEffect(() => {
-    const url = "https://dssj.stou.ac.th/api/data/fg?fac_code=10";
-
-    fetch(`${url}`, {
-      method: 'GET',
+    // const url = "http://localhost:3000/api/data";
+    
+    axios.get('https://dssj.stou.ac.th/api/data/fg?fac_code=10', {
       headers: {
         'Content-Type': 'application/json',
-        'api-token': '$2y$10$TY2vwRplhBQwy1b.nfI2y.PkQzBFb3mEtpkr1ptubTKZfPw6yH.Ae',
-      },
+        'api-token': '$2y$10$TY2vwRplhBQwy1b.nfI2y.PkQzBFb3mEtpkr1ptubTKZfPw6yH.Ae'
+      }
     })
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        const contentType = response.headers.get('content-type');
-        if (!contentType || !contentType.includes('application/json')) {
-          throw new TypeError("Oops, we haven't got JSON!");
-        }
-        return response.json();
-      })
-      .then(data => setData(data))
-      .catch(error => {
-        console.error('There has been a problem with your fetch operation: ', error);
-        setError(error.message);
-      });
+    .then(response => {
+      console.log(response.data);
+      setData(response.data);
+    })
+    .catch(error => {
+      console.error('Error fetching data: ', error);
+    });
+
+    // fetch(`${url}`, {
+    //   method: 'GET',
+    //   headers: {
+    //     'Content-Type': 'application/json',
+    //     'api-token': '$2y$10$TY2vwRplhBQwy1b.nfI2y.PkQzBFb3mEtpkr1ptubTKZfPw6yH.Ae',
+    //   },
+    // })
+    //   .then(response => {
+    //     if (!response.ok) {
+    //       throw new Error('Network response was not ok');
+    //     }
+    //     const contentType = response.headers.get('content-type');
+    //     if (!contentType || !contentType.includes('application/json')) {
+    //       throw new TypeError("Oops, we haven't got JSON!");
+    //     }
+    //     return response.json();
+    //   })
+    //   .then(data => setData(data))
+    //   .catch(error => console.error('There has been a problem with your fetch operation: ', error));
   }, []);
 
   const groupedData = data ? data.results.reduce((acc, item) => {
-    const key = item.group_name; // replace 'group_name' with the property you want to group by
+    const key = item.education_type_level_name; 
     if (!acc[key]) {
       acc[key] = [];
     }
@@ -41,69 +52,35 @@ const App = () => {
   }, {}) : {};
 
   return (
-    <div style={{ margin: 'auto 0'}}>
-    {error ? (
-      <div>Error: {error}</div>
-    ) : data ? (
-      Object.keys(groupedData).map((groupName, index) => (
-        <div key={index}>
-          <h2>{groupName}</h2>
-          <table>
-        <thead>
-          <tr>
-            <th>no.</th>
-            <th>ou</th>
-            <th>division_code</th>
-            <th>group_code</th>
-            <th>group_name</th>
-            <th>itm_type_code</th>
-            <th>type_name</th>
-            <th>fac_code</th>
-            <th>fac_name</th>
-            <th>education_type_level</th>
-            <th>education_type_level_name</th>
-            <th>subj_code</th>
-            <th>subj_name</th>
-            <th>rm_code</th>
-            <th>rm_name</th>
-            <th>ums_code</th>
-            <th>warehouse_code</th>
-            <th>bal_qty</th>
-            <th>bal_amt</th>
-          </tr>
-        </thead>
-        <tbody>
-        {groupedData[groupName].map((item, index) => (
-          <tr key={index}>
-            <td>{index+1}</td>
-            <td>{item.ou}</td>
-            <td>{item.division_code}</td>
-            <td>{item.group_code}</td>
-            <td>{item.group_name}</td>
-            <td>{item.itm_type_code}</td>
-            <td>{item.type_name}</td>
-            <td>{item.fac_code}</td>
-            <td>{item.fac_name}</td>
-            <td>{item.education_type_level}</td>
-            <td>{item.education_type_level_name}</td>
-            <td>{item.subj_code}</td>
-            <td>{item.subj_name}</td>
-            <td>{item.rm_code}</td>
-            <td>{item.rm_name}</td>
-            <td>{item.ums_code}</td>
-            <td>{item.warehouse_code}</td>
-            <td>{item.bal_qty}</td>
-            <td>{item.bal_amt}</td>
-            </tr>
+    <div>
+      {data ? (
+        Object.keys(groupedData).map((groupName, index) => (
+          <div key={index}>
+            <h2>{groupName} (Items: {groupedData[groupName].length})</h2>
+            <table>
+              <thead>
+                <tr>
+                  {groupedData[groupName][0] && Object.keys(groupedData[groupName][0]).map((key, index) => (
+                    <th key={index}>{key}</th>
+                  ))}
+                </tr>
+              </thead>
+              <tbody>
+              {groupedData[groupName].slice(0, 1).map((item, index) => (
+                <tr key={index}>
+                  {Object.values(item).map((value, index) => (
+                    <td key={index}>{value}</td>
+                  ))}
+                </tr>
               ))}
-            </tbody>
-          </table>
-        </div>
-      ))
-    ) : (
-      "Loading..."
-    )}
-  </div>
+              </tbody>
+            </table>
+          </div>
+        ))
+      ) : (
+        "Loading..."
+      )}
+    </div>
   );
 };
 
